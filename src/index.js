55172c5e427b95b0192842371e30d49b82f56a8b
Mohamed raushan Najumudeen <mohamedraushannajumudeen@Mohameds-MBP.modem>
2022-01-02 22:48:22 +1100

    t

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
+
 
 
 ReactDOM.render(
